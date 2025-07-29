68c8d0ba6f09739cfd204fbb0f34138629ef84f7
jaemin <jaemin4033@gmail.com>
2025-07-29 18:15:05 +0900

    api 경로 변경

--- src/main/java/dev/bigquery/big_query_server/controller/ContentController.java
+++ src/main/java/dev/bigquery/big_query_server/controller/ContentController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/api/contents")
+@RequestMapping("/contents")
 public class ContentController {
 
     @Autowired
