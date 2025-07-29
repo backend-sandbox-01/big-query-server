5baa293af12a880dfe521070b8dfb0d6be73d603
jaemin <jaemin4033@gmail.com>
2025-07-29 10:57:16 +0900

    init

---
+++ src/main/java/dev/bigquery/big_query_server/BigQueryServerApplication.java
@@ -0,0 +1,13 @@
+package dev.bigquery.big_query_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigQueryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigQueryServerApplication.class, args);
+	}
+
+}
